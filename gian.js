dc9688a3d867e997fbe5f0eb4d927ee4ce8e6a4b
Amansimlembe <168660420+Amansimlembe@users.noreply.github.com>
2024-05-31 19:22:36 +0300

    Add files via upload

---
+++ gian.js
@@ -0,0 +1,10 @@
+/*function openMenu() {
+    var x = document.getElementById("sidenav");
+    x.style.display = "block";
+ }
+ function closeMenu() {
+     var x = document.getElementById("sidenav");
+     x.style.display = "none";
+ }
+ javascript linking has shida....am tired.
+ */
